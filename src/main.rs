b170321fc48650531df0d79861d3044da3c6bde2
WolfgangTD <40742968+WolfgangTD@users.noreply.github.com>
2023-07-18 18:31:14 +1000

    added image maker

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,29 @@
 mod vector;
 
-use vector::{Vector3, Dot, Cross};
- 
+use vector::{Colour, WriteColour};
+
 fn main() {
-    let point1 = Vector3{
-        x: 1.0,
-        y: 2.0,
-        z: 3.0
-    };
-    let point2 = Vector3{
-        x: 4.0,
-        y: 5.0,
-        z: 6.0
-    };
-    let blahblah = point1.dot(&point2);
-    let result = point1.cross(&point2);
-    println!("{:?}", blahblah);
-    println!("{:?}", result);
+    let image_width:i32 = 256;
+    let image_height:i32 = 256;
+
+    println!("P3");
+    println!("{image_width} {image_height}");
+    println!("255");
+
+    let mut j = image_height-1;
+
+    while j >= 0 {
+        for i in 0..image_width {
+
+            let pixel_color = Colour {
+                r: i as f32/(image_width-1) as f32,
+                g: j as f32/(image_height-1) as f32,
+                b: 0.25,
+            };
+            pixel_color.write_colour();
+        }
+        j-=1;
+    }
+
+    
 }
--- src/vector.rs
+++ src/vector.rs
@@ -1,11 +1,18 @@
 use std::ops::{Add, Sub};
 
 #[derive(Debug)]
-pub struct Vector3 {
+pub struct Vector3{
     pub x: f32,
     pub y: f32,
     pub z: f32,
 }
+
+pub struct Colour{
+    pub r: f32,
+    pub g: f32,
+    pub b: f32,
+}
+
 
 impl Add for Vector3 {
     type Output = Self;
@@ -30,6 +37,14 @@ pub trait Normalize {
     fn normalize(&self) -> Vector3;
 }
 
+pub trait Scalar {
+    fn scalar(&self, scalar: f32) -> Vector3;
+}
+
+pub trait WriteColour {
+    fn write_colour(&self);
+}
+
 impl Dot for Vector3 {
     fn dot(&self, other: &Vector3) -> f32 {
         self.x + other.x + self.y + other.y + self.z + other.z
@@ -50,5 +65,21 @@ impl Normalize for Vector3 {
     fn normalize(&self) -> Vector3 {
         let magnitude: f32 = self.x*self.x + self.y*self.y + self.z*self.z;
         Vector3 { x: self.x/magnitude.sqrt(), y: self.y/magnitude.sqrt(), z: self.z/magnitude.sqrt() }
+    }
+}
+
+impl Scalar for Vector3 {
+    fn scalar(&self, other:f32) -> Vector3 {
+        Vector3 { x: self.x * other, y: self.y * other, z: self.z * other }
+    }
+}
+
+impl WriteColour for Colour {
+    fn write_colour(&self) {
+        let r = (255.999 * self.r) as i32;
+        let g = (255.999 * self.g) as i32;
+        let b = (255.999 * self.b) as i32;
+
+        println!("{r} {g} {b}");
     }
 }
