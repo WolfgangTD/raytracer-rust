91a60269eea40bc871d0ccae45fc0f3298107d23
WolfgangTD <40742968+WolfgangTD@users.noreply.github.com>
2023-07-19 18:25:41 +1000

    sky box yay :D

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,90 @@
 mod vector;
+use std::io::{stdout, Write};
+use vector::{WriteColour, Vector3, Scalar, Normalize};
 
-use vector::{Colour, WriteColour};
+struct Ray {
+    origin: Vector3,
+    dir: Vector3,
+}
 
+trait At {
+    fn at(r:Ray, t:f64) -> Vector3;
+}
+
+trait RayColour {
+    fn ray_colour(self) -> Vector3;
+}
+
+impl At for Ray {
+    fn at(r:Ray ,t:f64) -> Vector3{
+        r.origin + r.dir.scalar(t)
+    }
+}
+
+impl RayColour for Ray {
+    fn ray_colour(self) -> Vector3 {
+        let unit_direction = self.dir.normalize();
+        let t = 0.5*unit_direction.y + 1.0;
+        let colour1 = Vector3 {
+            x: 1.0,
+            y: 1.0,
+            z: 1.0
+        };
+        let colour2 = Vector3 {
+            x: 0.5,
+            y: 0.7,
+            z: 1.0
+        };
+        colour1.scalar(1.0-t) + colour2.scalar(t)
+    }
+}
 fn main() {
-    let image_width:i32 = 256;
-    let image_height:i32 = 256;
+    //image
+    let aspect_ratio = 16.0/9.0;
+    let image_width = 400;
+    let image_height = image_width / aspect_ratio as i32;
 
-    println!("P3");
-    println!("{image_width} {image_height}");
-    println!("255");
+    //camera
+    let viewport_height = 2.0;
+    let viewport_width = aspect_ratio * viewport_height;
+    let focal_length = 1.0;
+
+    let origin = Vector3 {
+        x: 0.0,
+        y: 0.0,
+        z: 0.0
+    };
+    let horizontal = Vector3 {
+        x: viewport_width,
+        y: 0.0,
+        z: 0.0
+    };
+    let vertical = Vector3 {
+        x: 0.0,
+        y: viewport_height,
+        z: 0.0
+    };
+    let lower_left_corner = origin - horizontal.scalar(1.0/2.0) - Vector3 { x: (0.0), y: (0.0), z: (focal_length) };
+
+    println!("P3\n{image_width} {image_height}\n255\n");
 
     let mut j = image_height-1;
 
     while j >= 0 {
+        eprintln!("\rScanlines remaining {j}");
+        stdout().flush().unwrap();
         for i in 0..image_width {
-
-            let pixel_color = Colour {
-                r: i as f32/(image_width-1) as f32,
-                g: j as f32/(image_height-1) as f32,
-                b: 0.25,
+            let u = (i / image_width-1) as f64; 
+            let v = (j/image_height-1) as f64;
+            let r = Ray {
+                origin: origin,
+                dir: lower_left_corner + horizontal.scalar(u) + vertical.scalar(v) - origin,
             };
-            pixel_color.write_colour();
+            let pixel_color = r.ray_colour();
+            
+            pixel_color.write_colour();     
         }
         j-=1;
     }
-
-    
+    eprintln!("\nDone.\n");
 }
--- src/vector.rs
+++ src/vector.rs
@@ -1,18 +1,11 @@
 use std::ops::{Add, Sub};
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 pub struct Vector3{
-    pub x: f32,
-    pub y: f32,
-    pub z: f32,
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
 }
-
-pub struct Colour{
-    pub r: f32,
-    pub g: f32,
-    pub b: f32,
-}
-
 
 impl Add for Vector3 {
     type Output = Self;
@@ -26,8 +19,9 @@ impl Sub for Vector3 {
         Self {x: self.x - other.x, y: self.y - other.y, z: self.z - other.z}
     }
 }
+
 pub trait Dot {
-    fn dot(&self, other: &Self) -> f32;
+    fn dot(&self, other: &Self) -> f64;
 }
 pub trait Cross {
     fn cross(&self, other: &Self) -> Vector3;
@@ -38,7 +32,7 @@ pub trait Normalize {
 }
 
 pub trait Scalar {
-    fn scalar(&self, scalar: f32) -> Vector3;
+    fn scalar(&self, scalar: f64) -> Vector3;
 }
 
 pub trait WriteColour {
@@ -46,7 +40,7 @@ pub trait WriteColour {
 }
 
 impl Dot for Vector3 {
-    fn dot(&self, other: &Vector3) -> f32 {
+    fn dot(&self, other: &Vector3) -> f64 {
         self.x + other.x + self.y + other.y + self.z + other.z
     }
 }
@@ -63,22 +57,24 @@ impl Cross for Vector3 {
 
 impl Normalize for Vector3 {
     fn normalize(&self) -> Vector3 {
-        let magnitude: f32 = self.x*self.x + self.y*self.y + self.z*self.z;
+        let magnitude: f64 = self.x*self.x + self.y*self.y + self.z*self.z;
         Vector3 { x: self.x/magnitude.sqrt(), y: self.y/magnitude.sqrt(), z: self.z/magnitude.sqrt() }
     }
 }
 
 impl Scalar for Vector3 {
-    fn scalar(&self, other:f32) -> Vector3 {
+    fn scalar(&self, other:f64) -> Vector3 {
         Vector3 { x: self.x * other, y: self.y * other, z: self.z * other }
     }
 }
 
-impl WriteColour for Colour {
+
+
+impl WriteColour for Vector3 {
     fn write_colour(&self) {
-        let r = (255.999 * self.r) as i32;
-        let g = (255.999 * self.g) as i32;
-        let b = (255.999 * self.b) as i32;
+        let r = (255.999 * self.x) as i32;
+        let g = (255.999 * self.y) as i32;
+        let b = (255.999 * self.z) as i32;
 
         println!("{r} {g} {b}");
     }
